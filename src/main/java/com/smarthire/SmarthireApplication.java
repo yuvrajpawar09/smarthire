a6c99f98d63d2f5a8b618dc83ef1003486a75b86
yuvrajpawar09 <payuvraj62@gmail.com>
2026-04-17 23:41:27 +0530

    Day 1: Spring Boot setup, MySQL connected, first API created

---
+++ src/main/java/com/smarthire/SmarthireApplication.java
@@ -0,0 +1,13 @@
+package com.smarthire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarthireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smarthire/SmarthireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarthireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
